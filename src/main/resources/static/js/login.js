28e5529441873ddac641ebd5089ffb047aafc86b
Ramón <ramonarj@ucm.es>
2019-03-18 15:37:12 +0100

    Cosas de la clase

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	
-	//document.getElementById("botonLogin").onclick = function()
-	//{
-	//	var idBombero = document.getElementById("idBombero").value;
-	//	var contraseñaBombero = document.getElementById("contraseñaBombero").value;
-	//	if(idBombero != "" && contraseñaBombero != "")
-	//	{
-	//		alert(contraseñaBombero);
-	//		document.location.href = '/inicio';
-	//	}
-	//}
+	document.getElementById("botonLogin").onclick = function()
+	{
+		var idBombero = document.getElementById("idBombero").value;
+		var contraseñaBombero = document.getElementById("contraseñaBombero").value;
+		if(idBombero != "" && contraseñaBombero != "")
+		{
+			document.location.href = "../templates/inicio";
+			alert(document.location.href);
+		}
+	}
 });
