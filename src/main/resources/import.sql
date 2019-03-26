9660bc3d8678b62ee030ae1313f23f4c63f2a60b
Ramón <ramonarj@ucm.es>
2019-03-26 14:49:37 +0100

    LA BASE DE DATOS HA SIDO ACTUALIZADA

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -35,18 +35,24 @@ INSERT INTO user(id,enabled,login,password,roles) VALUES (1, 1, 'a',
 INSERT INTO user(id,enabled,login,password,roles) VALUES (2, 1, 'p', 
 	'{bcrypt}$2a$04$5v02dQ.kxt7B5tJIA4gh3u/JFQlxmoCadSnk76PnvoN35Oz.ge3GK', 	'USER');
 -- a teacher with password 'q'
-INSERT INTO user(id,enabled,login,password,roles) VALUES (3, 1, 'q', 
+INSERT INTO user(id,enabled,login,password,roles) VALUES 
+(3, 1, 'q', 
 	'{bcrypt}$2a$04$9rrSETFYL/gqiBxBCy3DMOIZ6qmLigzjqnOGbsNji/bt65q.YBfjK', 	'USER');
-	
-INSERT INTO user(id,enabled,login,password,roles) VALUES (4, 1, 'ale', 
+-- NUUESTROS USUARIOS	
+INSERT INTO user(id,enabled,login,password,roles) VALUES 
+(4, 1, 'ale', '696969696', 'xXxXCuchilloNegroXxXx@yahoo.com', 'Er puerto de Santa María, Barca 17',
 	'{bcrypt}$2a$04$eiPCDkX7i.89wQl7DOEaMO7BYVQIDWLtZzAm1bLJpZ8l58/IE1mmi', 	'USER');
-INSERT INTO user(id,enabled,login,password,roles) VALUES (5, 1, 'ramon', 
+INSERT INTO user(id,enabled,login,phone,mail,address,password,roles) VALUES 
+(5, 1, 'ramon', '695274369', 'ramonarku@gmail.com', 'C/San Jacinto nº64, 3ºE', 
 	'{bcrypt}$2a$04$/92dKh3H3UJ1xizYfMpQMuxXVri60Xod6lMpMgIwSkV4mYX0pQ.Se', 	'USER');
-INSERT INTO user(id,enabled,login,password,roles) VALUES (6, 1, 'kelvin', 
+INSERT INTO user(id,enabled,login,password,roles) VALUES 
+(6, 1, 'kelvin', '123456789', 'erKelvinReshulon@hotmail.com', 'Avda. Ballecas nº404',
 	'{bcrypt}$2a$04$OudKz64nPFOcNC.UMgMYy.L45HCYb52AenVYqC1.X6X.oEXIR6vyG', 	'USER');
-INSERT INTO user(id,enabled,login,password,roles) VALUES (7, 1, 'alberto', 
+INSERT INTO user(id,enabled,login,password,roles) VALUES 
+(7, 1, 'alberto', '444666888', 'alberto360@gmail.net', 'Legaspi',
 	'{bcrypt}$2a$04$512BG9TBSwDBxdG8J7f.uOJ2OKkwwIxgk7dNYy0BwXrLgMQgz2TEG', 	'USER');
-INSERT INTO user(id,enabled,login,password,roles) VALUES (8, 1, 'javi', 
+INSERT INTO user(id,enabled,login,password,roles) VALUES
+ (8, 1, 'javi', '000123456', 'ElCorderitoCalvo@gmail.com', 'ExtremoDuro',
 	'{bcrypt}$2a$04$9AkKzXf94ipILs.3ZmcBi.f64auKWrycYLLPRwczR/I/MAJzF1pli', 	'USER');
 
 
