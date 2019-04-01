b08aa49a2f666cd49a80cebe47a4060aa1b43344
Ramón <ramonarj@ucm.es>
2019-04-01 15:22:46 +0200

    Map de admin

--- src/main/java/es/ucm/fdi/iw/LoginSuccessHandler.java
+++ src/main/java/es/ucm/fdi/iw/LoginSuccessHandler.java
@@ -63,7 +63,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 		
 		// redirects to 'admin' or 'user/{id}', depending on the user
 		response.sendRedirect(
-				u.hasRole("admin") ? "admin/" :
+				u.hasRole("admin") ? "admin/" + u.getId():
 				u.hasRole("student") ? "clase/" :
 					"user/" + u.getId());
 	}
