d115d4c87f0c05d21f363355971f1abbbb4e84ed
Alberto <albcor01@ucm.es>
2019-04-29 15:33:53 +0200

    cosas nuevas de webSockets

--- src/main/java/es/ucm/fdi/iw/control/UserController.java
+++ src/main/java/es/ucm/fdi/iw/control/UserController.java
@@ -76,7 +76,7 @@ public class UserController {
 		
 		Turno t = entityManager.find(Turno.class, id);
 		
-		model.addAttribute("dias", dias);
+		//model.addAttribute("dias", dias);
 		
 		//model.addAttribute("eventos", "[{\"title\":\"event1\",\"start\":\"2010-01-01\",\"end\":\"2010-01-02\"}]");
 		
